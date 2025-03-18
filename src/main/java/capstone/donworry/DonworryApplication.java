152836be95b70d90992efacb42cfae6a949f1888
choiyoung69 <choiyoung609@gmail.com>
2025-03-18 11:16:08 +0900

    feat: 전체 설정

---
+++ src/main/java/capstone/donworry/DonworryApplication.java
@@ -0,0 +1,13 @@
+package capstone.donworry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonworryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonworryApplication.class, args);
+	}
+
+}
