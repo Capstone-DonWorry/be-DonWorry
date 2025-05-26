d10e5998d8d64daf16ab456080f313cb16db15af
Yelim <yelim0s@naver.com>
2025-05-26 22:25:01 +0900

    feat:  securityConfig 설정

---
+++ src/main/java/capstone/donworry/login/common/SecurityConfig.java
@@ -0,0 +1,75 @@
+package capstone.donworry.login.common;
+
+import capstone.donworry.login.jwt.JwtAuthenticationFilter;
+import capstone.donworry.login.oauth.CustomOAuth2UserService;
+import capstone.donworry.login.oauth.OAuth2SuccessHandler;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final CustomOAuth2UserService customOauth2UserService;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final OAuth2SuccessHandler oAuth2SuccessHandler;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                                .requestMatchers("/api/admin/**").hasRole("ADMIN")
+                                .requestMatchers("/api/member/signup", "/api/jwt/login", "/api/oauth/login").permitAll()
+                                .anyRequest().authenticated()
+                );
+
+
+
+
+        http
+                .oauth2Login((auth) -> auth
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .userService(customOauth2UserService))
+                        .successHandler(oAuth2SuccessHandler)
+                        .failureUrl("/api/oauth/login-fail")
+                );
+
+        http
+                .exceptionHandling(e -> e
+                        .authenticationEntryPoint((request, response, authException) -> {
+                            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                            response.setContentType("application/json;charset=UTF-8");
+                            response.getWriter().write("{\"error\": \"Unauthorized access\"}");
+                        })
+                );
+
+
+        http
+                .logout((auth) -> auth
+                        .logoutUrl("api/oauth/logout")
+                        .deleteCookies("JSESSIONID"));
+
+
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
