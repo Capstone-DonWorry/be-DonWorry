b1298a593f917e72a22dfb94b02125cef56be2b4
Yelim <yelim0s@naver.com>
2025-05-26 22:20:46 +0900

    feat: loginDTO 생성

---
+++ src/main/java/capstone/donworry/login/common/LoginResponseDTO.java
@@ -0,0 +1,25 @@
+package capstone.donworry.login.common;
+
+import capstone.donworry.member.domain.Member;
+import capstone.donworry.member.domain.MemberRole;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class LoginResponseDTO {
+    private String token;
+    private String loginId;
+    private String name;
+    private MemberRole role;
+
+    public static LoginResponseDTO from(Member member, String token) {
+        return new LoginResponseDTO(
+                token,
+                member.getLoginId(),
+                member.getName(),
+                member.getRole()
+        );
+    }
+}
+
