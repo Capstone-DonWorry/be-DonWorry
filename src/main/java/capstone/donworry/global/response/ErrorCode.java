5cd5a9c50489fe6ac8af69bb7786d5607188b9dd
Yelim <yelim0s@naver.com>
2025-05-26 22:38:56 +0900

    feat: UNAUTHORIZED 에러추가

--- src/main/java/capstone/donworry/global/response/ErrorCode.java
+++ src/main/java/capstone/donworry/global/response/ErrorCode.java
@@ -15,7 +15,8 @@ public enum ErrorCode {
 
     //실패 response
     INVALID_REQUEST(STATUS_FAIL, HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
-    ENTITY_NOT_FOUND(STATUS_FAIL, HttpStatus.NOT_FOUND , "해당 데이터를 찾을 수 없습니다");
+    ENTITY_NOT_FOUND(STATUS_FAIL, HttpStatus.NOT_FOUND , "해당 데이터를 찾을 수 없습니다"),
+    UNAUTHORIZED(STATUS_FAIL, HttpStatus.UNAUTHORIZED, "권한이 없습니다.");
 
     private final String status;
     private final HttpStatus httpStatus;
